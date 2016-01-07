127f7429cc241566d509d8ed5fa99280a5bf060e
Mark Whitehorn <kd0aij@gmail.com>
2016-01-07 15:30:48 -0700

    fix sbus input for pixracer R07

--- src/drivers/px4fmu/fmu.cpp
+++ src/drivers/px4fmu/fmu.cpp
@@ -835,163 +835,79 @@ PX4FMU::cycle()
 
 	bool rc_updated = false;
 
-//#ifdef xxxx
-//	bool sbus_failsafe, sbus_frame_drop;
-//	uint16_t raw_rc_values[input_rc_s::RC_INPUT_MAX_CHANNELS];
-//	uint16_t raw_rc_count;
-//
-//	// read port
-//	bool sbus_updated = sbus_input(_sbus_fd, &raw_rc_values[0], &raw_rc_count,
-//			&sbus_failsafe, &sbus_frame_drop,
-//			input_rc_s::RC_INPUT_MAX_CHANNELS);
-//	if (sbus_updated) {
-//		//				dprint("fmu: sbus_input valid");
-//		warnx("%llu, %u, %u, %u, %u\n", hrt_absolute_time(), raw_rc_values[0], raw_rc_values[1],
-//				raw_rc_values[2], raw_rc_values[3]);
-//		// we have a new PPM frame. Publish it.
-//		_rc_in.channel_count = raw_rc_count;
-//
-//		if (_rc_in.channel_count > input_rc_s::RC_INPUT_MAX_CHANNELS) {
-//			_rc_in.channel_count = input_rc_s::RC_INPUT_MAX_CHANNELS;
-//		}
-//
-//		for (uint8_t i = 0; i < _rc_in.channel_count; i++) {
-//			_rc_in.values[i] = raw_rc_values[i];
-//		}
-//
-//		_rc_in.timestamp_publication = hrt_absolute_time();
-//		_rc_in.timestamp_last_signal = _rc_in.timestamp_publication;
-//
-//		_rc_in.rc_ppm_frame_length = 0;
-//		_rc_in.rssi = (!sbus_frame_drop) ?
-//		RC_INPUT_RSSI_MAX :
-//											(RC_INPUT_RSSI_MAX / 2);
-//		_rc_in.rc_failsafe = sbus_failsafe;
-//		_rc_in.rc_lost = false;
-//		_rc_in.rc_lost_frame_count = sbus_dropped_frames();
-//		_rc_in.rc_total_frame_count = 0;
-//
-//		rc_updated = true;
-//	}
-//#endif
 #ifdef RC_SERIAL_PORT
-	// This block scans for a supported serial RC input and locks onto the first one found
-	static hrt_abstime rc_scan_last_lock = 0;
-	static hrt_abstime rc_scan_begin = 0;
-//	static bool rc_scan_locked = false;
-	enum RC_SCAN _rc_scan_state = RC_SCAN_SBUS;
+	bool sbus_failsafe, sbus_frame_drop;
+	uint16_t raw_rc_values[input_rc_s::RC_INPUT_MAX_CHANNELS];
+	uint16_t raw_rc_count;
 
-	// Scan for one second, then switch protocol
-	constexpr hrt_abstime rc_scan_max = 1000 * 1000;
-	hrt_abstime now = hrt_absolute_time();
+	// read port
+	bool sbus_updated = sbus_input(_sbus_fd, &raw_rc_values[0], &raw_rc_count,
+			&sbus_failsafe, &sbus_frame_drop,
+			input_rc_s::RC_INPUT_MAX_CHANNELS);
+	if (sbus_updated) {
+		// we have a new SBUS frame. Publish it.
 
-	switch (_rc_scan_state) {
-	case RC_SCAN_SBUS:
-		if (rc_scan_begin == 0) {
-			rc_scan_begin = now;
-			warnx("fmu: RC_SCAN_SBUS: begin");
-			// Configure S.BUS port
-//			_sbus_fd = sbus_init(RC_SERIAL_PORT, true);
-		} else if (hrt_absolute_time() - rc_scan_last_lock < rc_scan_max
-				|| hrt_absolute_time() - rc_scan_begin < rc_scan_max) {
+		hrt_abstime now = hrt_absolute_time();
+		static hrt_abstime last_update = now;
 
-			bool sbus_failsafe, sbus_frame_drop;
-			uint16_t raw_rc_values[input_rc_s::RC_INPUT_MAX_CHANNELS];
-			uint16_t raw_rc_count;
+		warnx("%llu, %llu, %u, %u, %u, %u", now - last_update, now,
+				raw_rc_values[0], raw_rc_values[1], raw_rc_values[2],
+				raw_rc_values[3]);
+		last_update = now;
 
-			// read port
-			bool sbus_updated = sbus_input(_sbus_fd, &raw_rc_values[0],
-					&raw_rc_count, &sbus_failsafe, &sbus_frame_drop,
-					input_rc_s::RC_INPUT_MAX_CHANNELS);
-			if (sbus_updated) {
-				// we have a new SBUS frame. Publish it.
-				_rc_in.channel_count = raw_rc_count;
+		_rc_in.channel_count = raw_rc_count;
 
-				warnx("%llu, %llu, %u, %u, %u, %u\n", hrt_absolute_time() - rc_scan_last_lock, now,
-						raw_rc_values[0], raw_rc_values[1], raw_rc_values[2],
-						raw_rc_values[3]);
-				if (_rc_in.channel_count > input_rc_s::RC_INPUT_MAX_CHANNELS) {
-					_rc_in.channel_count = input_rc_s::RC_INPUT_MAX_CHANNELS;
-				}
-
-				for (uint8_t i = 0; i < _rc_in.channel_count; i++) {
-					_rc_in.values[i] = raw_rc_values[i];
-				}
-
-				_rc_in.timestamp_publication = hrt_absolute_time();
-				_rc_in.timestamp_last_signal = _rc_in.timestamp_publication;
-
-				_rc_in.rc_ppm_frame_length = 0;
-				_rc_in.rssi =
-						(!sbus_frame_drop) ?
-								RC_INPUT_RSSI_MAX : (RC_INPUT_RSSI_MAX / 2);
-				_rc_in.rc_failsafe = sbus_failsafe;
-				_rc_in.rc_lost = false;
-				_rc_in.rc_lost_frame_count = sbus_dropped_frames();
-				_rc_in.rc_total_frame_count = 0;
-
-				rc_updated = true;
-				rc_scan_last_lock = now;
-//				rc_scan_locked = true;
-			}
-			// a false return from sbus_input does _not_ necessarily mean a problem
-//			else {
-//				// This triggers the port re-configuration
-////				dprint("fmu: sbus read failed");
-//
-////				rc_scan_locked = false;
-//				rc_updated = false;
-//				// Scan the next protocol
-////				_rc_scan_state = RC_SCAN_DSM;
-//			}
+		if (_rc_in.channel_count > input_rc_s::RC_INPUT_MAX_CHANNELS) {
+			_rc_in.channel_count = input_rc_s::RC_INPUT_MAX_CHANNELS;
 		}
-		break;
-	case RC_SCAN_DSM:
-		warnx("fmu: scan_dsm\n");
 
-		break;
-	case RC_SCAN_SUMD:
+		for (uint8_t i = 0; i < _rc_in.channel_count; i++) {
+			_rc_in.values[i] = raw_rc_values[i];
+		}
 
-		break;
-	case RC_SCAN_ST24:
+		_rc_in.timestamp_publication = hrt_absolute_time();
+		_rc_in.timestamp_last_signal = _rc_in.timestamp_publication;
 
-		break;
+		_rc_in.rc_ppm_frame_length = 0;
+		_rc_in.rssi = (!sbus_frame_drop) ?
+		RC_INPUT_RSSI_MAX :
+											(RC_INPUT_RSSI_MAX / 2);
+		_rc_in.rc_failsafe = sbus_failsafe;
+		_rc_in.rc_lost = false;
+		_rc_in.rc_lost_frame_count = sbus_dropped_frames();
+		_rc_in.rc_total_frame_count = 0;
+
+		rc_updated = true;
 	}
-
-
 #endif
 
 #ifdef HRT_PPM_CHANNEL
-//	if (!rc_scan_locked) {
+	// see if we have new PPM input data
+	if ((ppm_last_valid_decode != _rc_in.timestamp_last_signal)
+			&& ppm_decoded_channels > 3) {
+		// we have a new PPM frame. Publish it.
+		_rc_in.channel_count = ppm_decoded_channels;
 
-		// see if we have new PPM input data
-		if ((ppm_last_valid_decode != _rc_in.timestamp_last_signal) &&
-			ppm_decoded_channels > 3) {
-			// we have a new PPM frame. Publish it.
-			_rc_in.channel_count = ppm_decoded_channels;
-
-			if (_rc_in.channel_count > input_rc_s::RC_INPUT_MAX_CHANNELS) {
-				_rc_in.channel_count = input_rc_s::RC_INPUT_MAX_CHANNELS;
-			}
-
-			for (uint8_t i = 0; i < _rc_in.channel_count; i++) {
-				_rc_in.values[i] = ppm_buffer[i];
-			}
-
-			_rc_in.timestamp_publication = ppm_last_valid_decode;
-			_rc_in.timestamp_last_signal = ppm_last_valid_decode;
-
-			_rc_in.rc_ppm_frame_length = ppm_frame_length;
-			_rc_in.rssi = RC_INPUT_RSSI_MAX;
-			_rc_in.rc_failsafe = false;
-			_rc_in.rc_lost = false;
-			_rc_in.rc_lost_frame_count = 0;
-			_rc_in.rc_total_frame_count = 0;
-
-			rc_updated = true;
+		if (_rc_in.channel_count > input_rc_s::RC_INPUT_MAX_CHANNELS) {
+			_rc_in.channel_count = input_rc_s::RC_INPUT_MAX_CHANNELS;
 		}
-//	}
 
+		for (uint8_t i = 0; i < _rc_in.channel_count; i++) {
+			_rc_in.values[i] = ppm_buffer[i];
+		}
+
+		_rc_in.timestamp_publication = ppm_last_valid_decode;
+		_rc_in.timestamp_last_signal = ppm_last_valid_decode;
+
+		_rc_in.rc_ppm_frame_length = ppm_frame_length;
+		_rc_in.rssi = RC_INPUT_RSSI_MAX;
+		_rc_in.rc_failsafe = false;
+		_rc_in.rc_lost = false;
+		_rc_in.rc_lost_frame_count = 0;
+		_rc_in.rc_total_frame_count = 0;
+
+		rc_updated = true;
+	}
 #endif
 
 	if (rc_updated) {
