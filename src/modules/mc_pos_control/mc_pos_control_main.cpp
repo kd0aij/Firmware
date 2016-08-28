5f8221c7a4a215138ca774fb8760aa6b2c6a96c2
Mark Whitehorn <kd0aij@gmail.com>
2016-08-28 11:49:30 -0600

    set _vel_sp(2) to zero when asserting _run_alt_control

--- src/modules/mc_pos_control/mc_pos_control_main.cpp
+++ src/modules/mc_pos_control/mc_pos_control_main.cpp
@@ -885,6 +885,7 @@ MulticopterPositionControl::control_manual(float dt)
 			if (!_alt_hold_engaged) {
 				if (_params.hold_max_z < FLT_EPSILON || fabsf(_vel(2)) < _params.hold_max_z) {
 					_alt_hold_engaged = true;
+					_vel_sp(2) = 0.0f;
 
 				} else {
 					_alt_hold_engaged = false;
@@ -901,6 +902,7 @@ MulticopterPositionControl::control_manual(float dt)
 			_vel_sp(2) = req_vel_sp_scaled(2);
 			_pos_sp(2) = _pos(2);
 		}
+		PX4_INFO("_vel_sp(2): %6.3f, _run_alt_control %d", (double)_vel_sp(2), _run_alt_control);
 	}
 }
 
